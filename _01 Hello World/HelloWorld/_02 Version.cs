77f3c8eb73cf857bdc563c084bd14fe379e9e7a7
progquartz <progquartz@gmail.com>
2019-08-28 19:41:12 +0900

    #02 ~03 Version ~ Data type

---
+++ _01 Hello World/HelloWorld/_02 Version.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class _02_Version
+    {
+        /*
+         *  c#은 .Net Framework 버전 및 Visual Studio 버전과 밀접한 관련성이 있다.
+         *  .Net Framework 의 가장 최신 버전을 4.8이며, c#은 7.3이 최신 버전이다.
+         * 
+         *  버전별 주요 기능들은 다음과 같다. 
+         *  C# 2.0 
+         *  - C# Generics 
+         *  - Anonymous Method
+         *  - Nullable Type
+         *  - Partial Type
+         *  - C# yield 키워드
+         *  - Delegate에 대한 Covariance / Contravariance
+         *  
+         *  C# 3.0
+         *  - Lambda Expression (람다식)
+         *  - Anonymous Type (익명 타입)
+         *  - Extension Method (확장 메서드)
+         *  - C# var 키워드 (implicit type)
+         *  - LINQ
+         *  - Expression Tree
+         *  
+         *  C# 4.0C
+         *  - # dynamic (Late binding)
+         *  - Named Argument
+         *  - Optional Argument
+         *  - Indexed Property
+         *  - 보다 쉬운 Office COM API 지원
+         *  
+         *  C# 5.0	
+         *  - C# async / await
+         *  - Caller Information
+         *  
+         *  C# 6.0	
+         *  - 널 조건 연산자 (Null-conditional operator)
+         *  - 문자열 내삽(內揷) 기능 (String Interpolation)
+         *  - Dictionary Initializer
+         *  - nameof 연산자
+         *  - using static 문
+         *  - catch/finally 블럭에서 await 사용
+         *  - Exception Filter 지원
+         *  - 자동 속성 초기자 (Auto-Property Initializer)
+         *  - 읽기전용 자동 속성 (Getter only)
+         *  - Expression-bodied member 사용
+         *  
+         *  C# 7.0	
+         *  - 패턴 매칭
+         *  - 튜플 (Tuple)
+         *  - 로컬 함수 (Local Function)
+         *  - out 파라미터
+         *  - 자리수 분리자와 이진 리터럴
+         *  - Deconstructor
+         *  - ref local 과 ref return
+         *  - async 커스텀 리턴 타입
+         *  - 향상된 Expression-bodied 표현
+         *  - throw expression
+         * 
+         *
+         * 
+         * 
+         * 
+         */
+    }
+}
---
+++ _02 Data Type/_02 Data Type/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Data_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = true; // 0과 1로 (1 이상도 가능은 함) 이루어진 것 (1바이트)
+
+            int i = 100; // 정수 (32비트/4바이트)
+            short sh = 100; // 정수 (16비트/2바이트) 최대 정수의 크기가 더 작음.
+            long lo = 100; // 정수 (64비트 / 8바이트) 최대 정수의 크기가 더 큼.
+            // 부호가 있는 경우 signed integar 라고 하고, 없는 양수만 필요한 경우는 unsigned 를 이용해주자.
+            uint ui = 100; // 신기하게 unsigned 가 u로 간결화되버렸다. 신기
+            ushort us = 100;
+            ulong ul = 100ul;
+
+            
+            double d = 3.141592d; // 소숫점이 있는 숫자.(64비트 / 8바이트)
+            float f = 3.141592f; //f를 끝에다가 적어야만함. 소숫점이 있는 숫자, (32비트/ 4바이트) double 보다 표현할수 있는 자리수가 작음.
+            decimal de = 3.141592m; // m 을 끝에다가 적어야만 함. 소숫점이 있는 숫자 (128비트 / 16바이트) double보다 표현할 수 있는 자리수가 큼.
+
+            int ix = null; // 본디 int는 value 타입이기 때문에, null 값을 가질 수 없다.
+            int? ix2 = null; // 하지만, ?를 넣으면 null 값이 사용이 가능해지는데.. 여기에서 ? 값이 뭔 짓을 하는지를 알아봐야 한다.
+
+            if(ix2 == null)
+            {
+                Console.WriteLine("ix2 is null");
+            }
+            else
+            {
+                Console.WriteLine(ix2.Value);
+            }
+            // 문자 관련은 Null 값 지정이 가능하다. (value 타입과 reference 타입.)
+            string s = "Hello"; // 문자열 (1바이트 * 개수 + 1)
+            string s1 = null;
+
+            char ch = '한'; // 한 문자. 유니코드의 경우 4바이트다.
+
+            byte by = 0x46; // 2바이트
+
+            object o = 123; //모든 데이터타입을 담을 수 있는 타입! (충격) 오토와 유사한건지 알아봐야함.
+        
+    
+        }
+    }
+}
